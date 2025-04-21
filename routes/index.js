47663dfc0b8a4d28efb1b61a1049e01905e504a5
Xiaofei Liu <yifayya@gmail.com>
2025-04-21 12:09:04 -0400

    PROJECT2

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,13 @@
-const express = require('express');
-const router = express.Router();
+const fs = require('fs');
+const path = require('path');
 
-router.get('/', function(req, res, next) {
-  res.render('index');
+router.get('/api/animals', (req, res) => {
+  const filePath = path.join(__dirname, '../public/data/animals.json');
+  fs.readFile(filePath, 'utf8', (err, jsonData) => {
+    if (err) {
+      console.error('Error reading animals.json:', err);
+      return res.status(500).json({ error: 'Failed to load data' });
+    }
+    res.json(JSON.parse(jsonData));
+  });
 });
-
-module.exports = router;
